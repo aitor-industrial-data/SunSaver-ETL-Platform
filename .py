81f18f49719dcaf325755e0e229b70db0aab832a
aitor-industrial-data <aitor166@hotmail.com>
2026-05-20 15:27:25 +0200

    Update20260520

--- .py
+++
@@ -1,4 +0,0 @@
-aws ecr get-login-password --region eu-south-2 | docker login --username AWS --password-stdin 610140802215.dkr.ecr.eu-south-2.amazonaws.com
-docker build -t sunsaver-etl .
-docker tag sunsaver-etl:latest 610140802215.dkr.ecr.eu-south-2.amazonaws.com/sunsaver-etl:latest
-docker push 610140802215.dkr.ecr.eu-south-2.amazonaws.com/sunsaver-etl:latest
